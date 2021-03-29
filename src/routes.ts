885c5a219b2e2b2ff87f75e7acc4ddaa0aa2fd2f
hotali7 <hotali0106@gmail.com>
2021-03-29 19:39:02 +1000

    Initial commit

---
+++ src/routes.ts
@@ -0,0 +1,46 @@
+import Home from "./pages/Home";
+import Dapp from "./pages/Dapp";
+import Detail from "./pages/Detail";
+import ComingSoon1 from "./pages/ComingSoon1";
+import ComingSoon2 from "./pages/ComingSoon2";
+
+const routes = [
+  {
+    path: "/",
+    exact: true,
+    title: "Home",
+    component: Home
+  },
+  {
+    path: "/home",
+    exact: true,
+    title: "Home",
+    component: Home
+  },
+  {
+    path: "/dapp",
+    exact: true,
+    title: "Dapp",
+    component: Dapp
+  },
+  {
+    path: "/detail",
+    exact: true,
+    title: "Detail",
+    component: Detail
+  },
+  {
+    path: "/coming-soon1",
+    exact: true,
+    title: "Coming Soon1",
+    component: ComingSoon1
+  },
+  {
+    path: "/coming-soon2",
+    exact: true,
+    title: "coming-soon2",
+    component: ComingSoon2
+  }
+];
+
+export default routes;
